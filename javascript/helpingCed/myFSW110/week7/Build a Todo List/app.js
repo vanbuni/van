c4fdefb70886b0acc238c5afe2459dab5eb08a03
Lavan Bradley <you@example.com>
2022-03-10 21:26:58 -0800

    test

--- javascript/helpingCed/myFSW110/week7/Build a Todo List/app.js
+++ javascript/helpingCed/myFSW110/week7/Build a Todo List/app.js
@@ -4,29 +4,40 @@ let title = document.getElementById('title')
 let description = document.getElementById('description')
 let btn = document.getElementById('btn')
 let todo_div = document.getElementById('todo_div')
+description.style.marginTop = '10px'
+
 
 function createTodo(arr){
+        
         todo_div.innerHTML = ''
         todo_div.style.visibility = 'visible'
         arr.forEach((todo,id)=>{
-        console.log('Todo:', todo)
-        console.log('ID:', id)
+       
         let todo_list_div = document.createElement('div')
         todo_list_div.style.border = '1px solid black'
         todo_list_div.style.marginTop = '5px'
         todo_div.appendChild(todo_list_div)
         let title_li = document.createElement('li')
         let description_li = document.createElement('li')
+        let completed_li = document.createElement('li')
         let done_btn = document.createElement('button')
         let delete_btn = document.createElement('button')
         delete_btn.innerHTML = 'DELETE'
         done_btn.innerHTML = 'COMPLETE'
         title_li.innerHTML = `Title: ${todo.Title}`
         description_li.innerHTML = `Description: ${todo.Description}`
+        completed_li.innerHTML = `Completed: ${todo.Completed}`
         title_li.style.listStyleType = 'none'
         description_li.style.listStyleType = 'none'
+        completed_li.style.listStyleType = 'none'
+        if (todo.Completed === 'Complete'){
+            title_li.style.textDecoration = 'line-through'
+            description_li.style.textDecoration = 'line-through'
+                
+        }
         todo_list_div.appendChild(title_li)
         todo_list_div.appendChild(description_li)
+        todo_list_div.appendChild(completed_li)
         todo_list_div.appendChild(done_btn)
         todo_list_div.appendChild(delete_btn)
         title.value =''
@@ -35,9 +46,25 @@ function createTodo(arr){
             deleteTodo(id)
         })
         done_btn.addEventListener("click", () =>{
-            title_li.style.textDecoration = 'line-through'
-            description_li.style.textDecoration = 'line-through'
+            
+            
+            
+            
+            if (todo.Completed === 'Not Completed'){
+                todo.Completed = 'Complete'
+                completed_li.innerHTML = `Completed: ${todo.Completed}`
+                title_li.style.textDecoration = 'line-through'
+                description_li.style.textDecoration = 'line-through'
+                
+            }
+            else if (todo.Completed === 'Complete'){
+            todo.Completed = 'Not Completed'
+            completed_li.innerHTML = `Completed: ${todo.Completed}`
+            title_li.style.textDecoration = 'none'
+            description_li.style.textDecoration = 'none'
+            }
         })
+        
     })
 }
 
@@ -45,60 +72,16 @@ function createTodo(arr){
 
 
 btn.addEventListener('click', ()=>{
-    todoArr.push({'Title' :title.value, 'Description': description.value})
-    console.log(todoArr)
+    todoArr.push({'Title' :title.value, 'Description': description.value, Completed: 'Not Completed'})
+    
     createTodo(todoArr)
 })
 function deleteTodo(index){
     todoArr.splice(index, 1)
-    console.log(todoArr)
+    
     todoList.innerHTML = ''
     createTodo(todoArr)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// function createTodo(arr){
-//     arr.forEach((todo,id)=>{
-//         let list = document.createElement('li')
-//         let title_span = document.createElement('span')
-//         let description_span = document.createElement('span')
-//         let btn = document.createElement('button')
-
-//         title_span.innerHTML = title.value
-//         description_span.innerHTML = description.value
-//         btn.innerHTML = 'DELETE'
-
-//         btn.addEventListener("click", () =>{
-//             deleteTodo(id)
-//         })
-
-//         list.appendChild(title_span)
-//         list.appendChild(description_span)
-//         list.appendChild(btn)
-//         todoList.appendChild(list)
-//         todoArr.push(list,title_span,description_span,btn)
-//     })
-// }
-
-// function deleteTodo(index){
-//     todoArr.splice(index, 1)
-//     createTodo(todoArr)
-// }
-// window.addEventListener("load", () =>{
-//     createTodo(todoArr)
-// })
-// btn.addEventListener('click', ()=>{
-//     createTodo()
-// })
 
 
